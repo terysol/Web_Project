afee80f836003a9b3e9afa81262fda345d88ce60
solmin <solminkim03@naver.com>
2021-05-15 12:57:08 +0900

    21.05.15 _ click event

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
+
 const title=document.querySelector('#title');
 // title.innerHTML="Hi! From JS";
 // title.style.color="red";
 // document.title="I own you now";
 
-function handleResize(event){
-   title.style.color="red";
+const BASE_COLOR="rgb(52, 73, 94)";
+const OTHER_COLOR="#e056fd";
+
+function handleClick(event){
+   const currentColor=title.style.color;
+   if(currentColor === BASE_COLOR){
+       title.style.color=OTHER_COLOR;
+   }else{
+       title.style.color=BASE_COLOR;
+   }
+}
+function init(){
+    title.style.color=BASE_COLOR;
+    title.addEventListener("click", handleClick);
 }
 
-title.addEventListener("click",handleResize);
+init();
+
+// title.addEventListener("click",handleResize);
 // 함수 뒤에 괄호가 있으면 바로 호출하라는 뜻, 없으면 바로 호출 하지 않는다. 
+
+
+
