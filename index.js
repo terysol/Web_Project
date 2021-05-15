9ee67d831032c3e3177f7c4b4b4ffd927e2d0209
solmin <solminkim03@naver.com>
2021-05-15 12:44:13 +0900

    21.05.15 _ js 기본 문법

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("I'm Working . I'm Js, ")
+const title=document.querySelector('#title');
+// title.innerHTML="Hi! From JS";
+// title.style.color="red";
+// document.title="I own you now";
+
+function handleResize(event){
+   title.style.color="red";
+}
+
+title.addEventListener("click",handleResize);
+// 함수 뒤에 괄호가 있으면 바로 호출하라는 뜻, 없으면 바로 호출 하지 않는다. 
